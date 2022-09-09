cb5a070096a54a3b535643d3a6fbd9716e227e29
vlad1slav <vvlad1slavv@yandex.ru>
2022-09-09 21:51:32 +0300

    update 222

--- rover.py
+++ rover.py
@@ -2,9 +2,11 @@ import RPi.GPIO as GPIO
 from threading import Thread
 from time import sleep
 from motor import Motor
-from cleaner import Cleaner
 import socket
 import numpy as np
+
+EVAL_PC_HOST = '192.168.1.160'
+EVAL_PC_PORT = 7777
 
 class Rover(Thread):
 
@@ -16,71 +18,69 @@ class Rover(Thread):
 
         self.__MIN_BORDER = 85
         self.__MAX_BORDER = 100
-        #self.right_motor = Motor(27, 10, 19, 100)
-        #self.left_motor = Motor(24, 18, 26, 100)
-    
-    def __del__():
+        # self.right_motor = Motor(27, 10, 19, 100)
+        # self.left_motor = Motor(24, 18, 26, 100)
+        self.client_socket = None
+
+    def __del__(self):
         GPIO.cleanup()
+        if self.client_socket:
+            self.client_socket.close()
 
-    #will wait until connections is established
-    def init_server_socket(self, port:int):
-        self.server_socket = socket.socket()
-        self.server_socket.bind(('', port))
-        
-        print("Waiting for connection...")
-        self.server_socket.listen(1)
-        self.conn, self.addr = self.server_socket.accept()
-        print("Connected: ", self.addr) 
+    # will wait until connections is established
+    def init_client_socket(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((EVAL_PC_HOST, EVAL_PC_PORT))
 
-    #message type is np.array 1x2 size [linear_speed, angular_speed]
+    # message type is np.array 1x2 size [linear_speed, angular_speed]
     def get_message(self):
-        message = np.frombuffer(self.conn.recv(88888), np.uint8)
+        message = np.frombuffer(self.client_socket.recv(10000), np.float64)
         return message
-    
-    def close_connection(self):
-        self.conn.close()
 
-    def init_left_motor(self, en1:int, en2:int, enable:int):
+    def close_connection(self):
+        self.client_socket.close()
+
+    def init_left_motor(self, en1: int, en2: int, enable: int):
         self.left_motor = Motor(en1, en2, enable, 100)
         self.left_motor.enable_motor()
 
-    def init_right_motor(self, en1:int, en2:int, enable:int):
+    def init_right_motor(self, en1: int, en2: int, enable: int):
         self.right_motor = Motor(en1, en2, enable, 100)
         self.right_motor.enable_motor()
-    
+
     def disable_motors(self):
         self.right_motor.disable_motor()
         self.left_motor.disable_motor()
 
     def to_string(self):
         print(self.name)
-    
+
     def get_right_motors_pins(self):
-        print("en_1 : " + str(self.en1_right) + " " + "en_2 : " + str(self.en2_right) + "  " + "enable : " + str(self.enable_right))
+        print("en_1 : " + str(self.en1_right) + " " + "en_2 : " + str(self.en2_right) + "  " + "enable : " + str(
+            self.enable_right))
 
-
-    def __less_than_min_border(self, dc_val:int = 0):
+    def __less_than_min_border(self, dc_val: int = 0):
         if dc_val < self.__MIN_BORDER:
             return True
         else:
             return False
 
-    def __more_than_max_border(self, dc_val:int = 0):
+    def __more_than_max_border(self, dc_val: int = 0):
         if dc_val > self.__MAX_BORDER:
             return True
-        else: 
+        else:
             return False
 
     def stop_rover(self):
         self.__set_right_dc(0)
         self.__set_left_dc(0)
 
-    def set_speed(self, linear_sp:int, angular_sp:int):
+    def set_speed(self, linear_sp: int, angular_sp: int):
         if self.__less_than_min_border(linear_sp):
             linear_sp = self.__MIN_BORDER
             right_sp = linear_sp + angular_sp
             left_sp = linear_sp - angular_sp
-        else: 
+        else:
             right_sp = linear_sp + angular_sp
             left_sp = linear_sp - angular_sp
 
@@ -93,45 +93,46 @@ class Rover(Thread):
         self.__set_right_dc(right_sp)
         self.__set_left_dc(-left_sp)
 
-    def __set_right_dc(self, new_right_dc:int):
+    def __set_right_dc(self, new_right_dc: int):
         self.dc_right = new_right_dc
 
-    def __set_left_dc(self, new_left_dc:int):
+    def __set_left_dc(self, new_left_dc: int):
         self.dc_left = new_left_dc
-    
 
     def run(self):
-        while(1):
+        while (1):
             self.right_motor.set_dc(self.dc_right)
             self.left_motor.set_dc(self.dc_left)
+
 
 if __name__ == "__main__":
     rover = Rover()
     rover.to_string()
     rover.init_right_motor(27, 10, 19)
     rover.init_left_motor(24, 18, 26)
-    rover.init_server_socket(9999)
+    rover.init_client_socket(9000)
     rover.setDaemon(True)
     rover.start()
     try:
-        while(1):
+        while (1):
             message = rover.get_message()
+            print(message)
             if message.size == 0:
                 print("Emty data received!")
             # get data from camera
             else:
                 rover.set_speed(message[0], 0)
-           # sleep(2)
-           # rover.set_left_dc(85)
-           # rover.set_right_dc(85)
-           # sleep(2)
+        # sleep(2)
+        # rover.set_left_dc(85)
+        # rover.set_right_dc(85)
+        # sleep(2)
     except KeyboardInterrupt:
         rover.stop_rover()
-        #rover.set_right_dc(0)
+        # rover.set_right_dc(0)
         rover.disable_motors()
         rover.close_connection()
-    #rover.move_rover.set_rigrover.set_right_dc(50) ht_dc(50) right_motors_forward()
-    #sleep(5)
-    #rover.stop_motors()
+    # rover.move_rover.set_rigrover.set_right_dc(50) ht_dc(50) right_motors_forward()
+    # sleep(5)
+    # rover.stop_motors()
 
 
---
+++ test/test_server.py
@@ -0,0 +1,17 @@
+import socket
+import numpy as np
+import time
+
+HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
+PORT = 7777  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            conn.sendall(np.array([0.0, 1.0]).tobytes())
+            time.sleep(1.0)
+
